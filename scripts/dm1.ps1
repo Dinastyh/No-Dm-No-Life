eba075f5498b94d08c16b4e5454432530744e1d9
Paul Lege <paul.lege@epita.fr>
2023-02-28 12:26:55 +0100

    scripts: add informations about default web browser in dm1.ps1

--- scripts/dm1.ps1
+++ scripts/dm1.ps1
@@ -25,7 +25,8 @@ $adapterInfo = foreach ($adapter in $adapters)
 }
 
 # Get configured proxy
-$proxy = (Get-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings').ProxyServer
+$proxyPath = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings'
+$proxy = (Get-ItemProperty -Path $proxyPath).ProxyServer
 
 # Get configured VPN
 $vpn = Get-VpnConnection | Select-Object -ExpandProperty Name
@@ -44,17 +45,57 @@ $net = [PSCustomObject]@{
   DomainName = $domainName
   computerName = $computerName
 }
+
 #Miss wifi 
 
+#------------- Default Web-Browser -------------#
+
+# Get the default web browser version
+$startupMenuPath = 'HKLM:\SOFTWARE\Clients\StartMenuInternet'
+$defaultInternetPath = 'HKLM:\SOFTWARE\Clients\StartMenuInternet'
+$browserVersion = Get-ItemProperty  $startupMenuPath | Select-Object -ExpandProperty (Get-ItemProperty $defaultInternetPath | Where-Object {$_.Default}).Version
+
+# Get the names of installed browser add-ons
+$addOnsPath = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Ext\Settings'
+$addOns = Get-ChildItem $addOnsPath | Select-Object -ExpandProperty Name
+
+# Get the thumbprints of installed user certificates
+$certificatesPath = 'Cert:\CurrentUser\My'
+$certificates = Get-ChildItem $certificatesPath | Select-Object -ExpandProperty Thumbprint
+
+# Get the names of registered identifiers
+$identifiersPath = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\IdentityCRL\StoredIdentities'
+$identifiers = Get-ChildItem $identifiersPath | Select-Object -ExpandProperty Name
+
+# Get the URLs of user favorites
+$favoritesPath = "$env:USERPROFILE\Favorites"
+$favorites = (Get-ChildItem $favoritesPath -Recurse -Include *.url).FullName
+
+# Get the URLs and titles of browser history
+$historyPath = "$env:LOCALAPPDATA\Microsoft\Windows\WebCache\V01"
+$history = Get-ChildItem $historyPath -Recurse | Where-Object {$_.Name -eq "WebCacheV01.dat"} | Select-Object -ExpandProperty FullName | ForEach-Object {((New-Object -ComObject 'Shell.Application').NameSpace('shell:').ParseName($_).GetFolder).Items()} | Select-Object -Property Name, Folder
+
+$defaultWebBrowser = [PSCustomObject]@{
+  browserVersion = $browserVersion
+  addOns = $addOns
+  certificates = $certificates
+  identifiers = $identifiers
+  favorites = $favorites
+  history = $history
+}
+
+
+#------------- Firefox -------------#
 
 
 
-
+#------------- Export json -------------#
 
 # Recap object
-$final_object = [PSCustomObject]@{
+$finalObject = [PSCustomObject]@{
   Net = $net
+  DefaultWebBrowser = $defaultWebBrowser
 }
 
 # Save data as json file
-ConvertTo-Json $final_object | Set-Content result.json
+ConvertTo-Json $finalObject | Set-Content result.json
